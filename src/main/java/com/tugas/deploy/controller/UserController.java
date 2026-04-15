f883493b06d551b5d509f2f511e2691ade4fd2eb
abimanyusatriaa <adji3890@gmail.com>
2026-04-15 10:11:10 +0700

    menambah code usercontroller

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,33 +1,53 @@
 package com.tugas.deploy.controller;
 
+import com.tugas.deploy.model.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
-    private final String USERNAME = "admin123";
-    private final String PASSWORD = "12345";
+    private final Model model = new Model(); // buat objek Model
 
     @GetMapping("/")
-    public String loginPage() {return "login";}
+    public String loginPage() {
+        return "login";
+    }
 
     @PostMapping("/login")
     public String login(@RequestParam String username,
                         @RequestParam String password,
-                        Model model) {
-        if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+                        org.springframework.ui.Model uiModel) { // pakai fully qualified name
+
+        if (model.checkLogin(username, password)) {
             return "home";
         } else {
-            model.addAttribute("error",
-                    "Username atau Password Salah!");
+            uiModel.addAttribute("error", "Username atau Password Salah!");
             return "login";
         }
     }
-    @GetMapping ("/home")
-    public String home() {return "home";}
-}
 
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+    @PostMapping("/submit")
+    public String result(@RequestParam String nama,
+                         @RequestParam String nim,
+                         @RequestParam String jenisKelamin,
+                         org.springframework.ui.Model uiModel) {
+
+        com.tugas.deploy.model.Model mhs =
+                new com.tugas.deploy.model.Model(nama, nim, jenisKelamin);
+        com.tugas.deploy.model.Model.addMahasiswa(mhs);
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(org.springframework.ui.Model uiModel) {
+        uiModel.addAttribute("mahasiswaList",
+                com.tugas.deploy.model.Model.getMahasiswaList());
+        return "home";
+    }
+}
